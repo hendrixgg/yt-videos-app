f6f167c1c4422cf1f4ddfff7741908b2e62f3f5f
hendrixgg <hendrixg10@gmail.com>
2023-03-21 12:15:02 -0400

    formatting code

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 import useVideos from "../hooks/useVideos";
+
 const App = () => {
     const [selectedVideo, setSelectedVideo] = useState(null);
     const [videos, search] = useVideos("parkour");
